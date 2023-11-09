8fffaf63a4bea064740f7efe4e67f3ed2cb83b28
ihebKhodja <iheb.khodja@gmail.com>
2023-11-09 16:13:35 +0100

    updates

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5173'],
+    'allowed_origins' => ['http://127.0.0.1:5173','https://ecom-api-78v4.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
