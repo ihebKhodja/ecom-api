1f9857b9e6970ca06a1203fb6537295b22a0a097
ihebKhodja <iheb.khodja@gmail.com>
2024-01-22 11:09:20 +0100

    deployement

--- config/database.php
+++ config/database.php
@@ -45,23 +45,37 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('MYSQLHOST'), // Use the value of RAILWAY_TCP_PROXY_DOMAIN as the host
+            'port' => env('MYSQLPORT'), // Use the value of RAILWAY_TCP_PROXY_PORT as the port
+            'database' => env('MYSQLDATABASE'), // Use the value of MYSQL_DATABASE as the database name
+            'username' => env('MYSQLUSER'), // Use the value of MYSQLUSER as the username
+            'password' => env('MYSQLPASSWORD'), // Use the value of MYSQLPASSWORD as the password
+            'unix_socket' => env('DB_SOCKET', ''), // You may need to adjust this based on your environment
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
 
         'pgsql' => [
             'driver' => 'pgsql',
